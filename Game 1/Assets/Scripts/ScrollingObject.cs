3a91e4feb2d7916af05cd623a111e5e59ba08fc4
calebp <calebp@tamu.edu>
2017-09-03 16:36:57 -0500

    Background Setup
    Created a camera-wide background and a duplicate and a script that I thought was going to allow for a looping background but I realized it doesn't follow player character.

---
+++ Game 1/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+    private Rigidbody2D body;
+    public float scrollSpeed = -1.5f;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(scrollSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//  Maybe add something that stops scrolling when gameover?
+
+	}
+}
