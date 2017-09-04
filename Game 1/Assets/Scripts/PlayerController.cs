2e0f42908c0c21237d8c73995d89a1547b81b9b1
calebp <calebp@tamu.edu>
2017-09-04 11:33:52 -0500

    Removed auto-scroll, set up initial environment
    Also added a rocket feature

---
+++ Game 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  Simple script for a test I need to do
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpPower;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpPower);
+		}
+		
+	}
+}
