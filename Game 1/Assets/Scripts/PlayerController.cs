f8594399150cc3e67a923b64ddbe2f9c3a121a6e
calebp <calebp@tamu.edu>
2017-09-04 16:39:17 -0500

    Explosion work

--- Game 1/Assets/Scripts/PlayerController.cs
+++ Game 1/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float jumpPower;
+    public AnimationClip explosion;
+    private AnimationClip explode;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        explode = (AnimationClip)Instantiate(explosion, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        //  Basic jumping
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpPower);
 		}
-		
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            explode.Play();
+            Debug.Log("Explosion at: " + Input.mousePosition);
+        }
+                	
 	}
 }
