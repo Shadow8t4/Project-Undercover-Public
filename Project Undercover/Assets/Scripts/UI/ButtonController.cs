6d96db65a896f1baf807aefc511114f8deb977e9
darrelmarek <darrelmarek@tamu.edu>
2017-11-30 10:25:04 -0600

    Instructions

--- Project Undercover/Assets/Scripts/UI/ButtonController.cs
+++ Project Undercover/Assets/Scripts/UI/ButtonController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonController : MonoBehaviour {
+
+	public GameObject ui;
+
 	public void ExitGame() {
 		Application.Quit();
 	}
-	
+
 	public void NextScene(string name) {
 		Application.LoadLevel(name);
+	}
+
+	public void ShowInstructions() {
+		ui.SetActive (true);
+	}
+
+	public void HideInstructions() {
+		ui.SetActive (false);
 	}
 }
