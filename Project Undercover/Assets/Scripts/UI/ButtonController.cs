a20a910f1da0fc8b75a42fbe45695b782b45009e
shadow8t4 <shadow8t4@tamu.edu>
2017-11-30 14:32:12 -0600

    Button soudnds

--- Project Undercover/Assets/Scripts/UI/ButtonController.cs
+++ Project Undercover/Assets/Scripts/UI/ButtonController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour {
 
-	public GameObject ui;
+    public AudioClip button_sound;
+    public AudioSource button_sound_src;
+    public GameObject ui;
+
+    public void Start() {
+        button_sound_src = GetComponent<AudioSource>();
+    }
 
 	public void ExitGame() {
+        AudioManager.Main.PlayNewSound("button_sound");
 		Application.Quit();
 	}
 
-	public void NextScene(string name) {
-		Application.LoadLevel(name);
+	public void NextScene(string name)
+    {
+        AudioManager.Main.PlayNewSound("button_sound");
+        Application.LoadLevel(name);
 	}
 
 	public void ShowInstructions() {
-		ui.SetActive (true);
+
+        AudioManager.Main.PlayNewSound("button_sound");
+        ui.SetActive (true);
 	}
 
 	public void HideInstructions() {
-		ui.SetActive (false);
+
+        AudioManager.Main.PlayNewSound("button_sound");
+        ui.SetActive (false);
 	}
 }
