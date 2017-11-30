07a3822d16b1dda5fb6641a68c4085e2f59b3b4b
darrelmarek <darrelmarek@tamu.edu>
2017-11-30 11:10:59 -0600

    Added in game menu

---
+++ Project Undercover/Assets/Scripts/UI/InGameMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenuController : MonoBehaviour {
+
+	public GameObject inGameMenu;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape ))
+			EscapePressed();
+	}
+
+	void EscapePressed() {
+		inGameMenu.SetActive (true);
+	}
+
+	public void Resume() {
+		inGameMenu.SetActive (false);
+	}
+}
